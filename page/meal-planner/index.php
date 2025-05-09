52269196f26bc1c475f1e9a10681f438de837fc7
mm-gonzales <michaela.v.gonzales@gmail.com>
2025-05-09 20:24:35 +0800

    feat(meal-planner): add stylesheet link to index.php

--- page/meal-planner/index.php
+++ page/meal-planner/index.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title>Meal Planner</title>
+    <style rel="stylesheet" href=".assets/css/style.css"></style>
 </head>
 <body>
 
