e00e13a4520a8a70f22408485025004b907b8d13
mm-gonzales <michaela.v.gonzales@gmail.com>
2025-05-12 13:46:56 +0800

    feat(landing): add navbar structure to index.php

--- index.php
+++ index.php
@@ -9,12 +9,21 @@
 </head>
 <body>
 
+<nav class="navbar navbar-expand-lg custom-navbar">
+
+</nav>
+
+
+
     <div class="container-fluid landing-wrapper">
         <div class="landing-box">
             <h1 class="mb-4">Welcome to Meal Planner</h1>
             <a href="./page/meal-planner/index.php" class="btn btn-warning fw-bold">View Weekly Plan</a>
         </div>
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 
     <script src="./assets/js/script.js"></script>
 </body>
